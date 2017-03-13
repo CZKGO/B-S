19c7941dcd201ecf55648d20f03a87f15f5452cf
chenzhongkai <359992539@qq.com>
2017-03-13 21:35:45 +0800

    更换键盘方式，未完成

--- Diabetes-BS/app/src/main/java/com/czk/diabetes/view/Digitalkeyboard.java
+++ Diabetes-BS/app/src/main/java/com/czk/diabetes/view/Digitalkeyboard.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.czk.diabetes.R;
 
@@ -14,6 +15,18 @@ import com.czk.diabetes.R;
  */
 public class Digitalkeyboard extends LinearLayout implements View.OnClickListener {
     private static final String NAMESPACE = "http://schemas.android.com/apk/res/diabetes";
+    private TextView tvNumber0;
+    private TextView tvNumber1;
+    private TextView tvNumber2;
+    private TextView tvNumber3;
+    private TextView tvNumber4;
+    private TextView tvNumber5;
+    private TextView tvNumber6;
+    private TextView tvNumber7;
+    private TextView tvNumber8;
+    private TextView tvNumber9;
+    private TextView tvNumberDelete;
+    private TextView tvNumberPoint;
     public Digitalkeyboard(Context context) {
         super(context);
         initView();
@@ -38,6 +51,19 @@ public class Digitalkeyboard extends LinearLayout implements View.OnClickListene
 
     private void initView() {
         View.inflate(getContext(), R.layout.view_digital_keyboard, this);
+
+        tvNumber0 = (TextView) findViewById(com.czk.diabetes.R.id.number_0);
+        tvNumber1 = (TextView) findViewById(com.czk.diabetes.R.id.number_1);
+        tvNumber2 = (TextView) findViewById(com.czk.diabetes.R.id.number_2);
+        tvNumber3 = (TextView) findViewById(com.czk.diabetes.R.id.number_3);
+        tvNumber4 = (TextView) findViewById(com.czk.diabetes.R.id.number_4);
+        tvNumber5 = (TextView) findViewById(com.czk.diabetes.R.id.number_5);
+        tvNumber6 = (TextView) findViewById(com.czk.diabetes.R.id.number_6);
+        tvNumber7 = (TextView) findViewById(com.czk.diabetes.R.id.number_7);
+        tvNumber8 = (TextView) findViewById(com.czk.diabetes.R.id.number_8);
+        tvNumber9 = (TextView) findViewById(com.czk.diabetes.R.id.number_9);
+        tvNumberDelete = (TextView) findViewById(com.czk.diabetes.R.id.number_delete);
+        tvNumberPoint = (TextView) findViewById(com.czk.diabetes.R.id.number_point);
     }
 
     @Override
