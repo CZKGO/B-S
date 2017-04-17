7ff4f0d1f232a8a0156fc2d2b2b984849bbdba73
chenzhongkai <359992539@qq.com>
2017-04-17 21:32:01 +0800

    更换阴影

--- Diabetes-BS/app/src/main/java/com/czk/diabetes/notification/AddNotificationActivity.java
+++ Diabetes-BS/app/src/main/java/com/czk/diabetes/notification/AddNotificationActivity.java
@@ -4,15 +4,40 @@ import android.os.Bundle;
 
 import com.czk.diabetes.BaseActivity;
 import com.czk.diabetes.R;
+import com.czk.diabetes.view.SpinerPopWindow;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by 陈忠凯 on 2017/4/8.
  */
 
 public class AddNotificationActivity extends BaseActivity {
+    private SpinerPopWindow spwDate;
+    private List<String> dateChose;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_notifiction);
+        initData();
+        initView();
+        dealEvent();
+    }
+
+    private void initData() {
+        dateChose = Arrays.asList(getResources().getStringArray(R.array.date_chose));
+    }
+
+    private void initView() {
+        //初始化PopWindow
+        spwDate = new SpinerPopWindow(AddNotificationActivity.this);
+//        SpinerPopWindow.SpinerAdapter mSpinerAdapter = new SpinerPopWindow.SpinerAdapter();
+//        spwDate.setAdatper(mSpinerAdapter);
+    }
+
+    private void dealEvent() {
+        spwDate.setItemListener();
     }
 }
---
+++ Diabetes-BS/app/src/main/java/com/czk/diabetes/view/SpinerPopWindow.java
@@ -0,0 +1,157 @@
+package com.czk.diabetes.view;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.czk.diabetes.R;
+
+import java.util.List;
+
+/**
+ * Created by 陈忠凯 on 2017/4/17.
+ */
+
+public class SpinerPopWindow extends PopupWindow implements AdapterView.OnItemClickListener {
+
+    private Context mContext;
+    private ListView mListView;
+    private SpinerPopWindow.SpinerAdapter mAdapter;
+    private SpinerPopWindow.SpinerAdapter.IOnItemSelectListener mItemSelectListener;
+
+
+    public SpinerPopWindow(Context context){
+        super(context);
+        mContext = context;
+        init();
+    }
+
+
+    public void setItemListener(SpinerPopWindow.SpinerAdapter.IOnItemSelectListener listener){
+        mItemSelectListener = listener;
+    }
+
+    public void setAdatper(SpinerPopWindow.SpinerAdapter adapter){
+        mAdapter = adapter;
+        mListView.setAdapter(mAdapter);
+    }
+
+
+    private void init(){
+        View view = LayoutInflater.from(mContext).inflate(R.layout.spiner_window_layout, null);
+        setContentView(view);
+        setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        setFocusable(true);
+        //去掉黑边
+        ColorDrawable dw = new ColorDrawable(0x00);
+        setBackgroundDrawable(dw);
+
+        mListView = (ListView) view.findViewById(R.id.listview);
+        mListView.setOnItemClickListener(this);
+    }
+
+
+    public void refreshData(List<String> list, int selIndex){
+        if (list != null && selIndex  != -1)
+        {
+            if (mAdapter != null){
+                mAdapter.refreshData(list, selIndex);
+            }
+        }
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View view, int pos, long arg3) {
+        dismiss();
+        if (mItemSelectListener != null){
+            mItemSelectListener.onItemClick(pos);
+        }
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
+    }
+
+    /**
+     * Created by 陈忠凯 on 2017/4/17.
+     */
+
+    public static class SpinerAdapter extends BaseAdapter {
+
+        public  interface IOnItemSelectListener {
+            void onItemClick(int pos);
+        }
+
+        private List<String> mObjects;
+
+        private LayoutInflater mInflater;
+
+        public SpinerAdapter(Context context, List<String> mObjects) {
+            this.mObjects = mObjects;
+            mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+
+        public void refreshData(List<String> objects, int selIndex) {
+            mObjects = objects;
+            if (selIndex < 0) {
+                selIndex = 0;
+            }
+            if (selIndex >= mObjects.size()) {
+                selIndex = mObjects.size() - 1;
+            }
+        }
+
+
+        @Override
+        public int getCount() {
+
+            return mObjects.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return mObjects.get(pos).toString();
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup arg2) {
+            ViewHolder viewHolder;
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.item_spinner, null);
+                viewHolder = new ViewHolder();
+                viewHolder.mTextView = (TextView) convertView.findViewById(R.id.name);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            //Object item =  getItem(pos);
+            viewHolder.mTextView.setText(mObjects.get(pos));
+
+            return convertView;
+        }
+
+
+        public static class ViewHolder {
+            public TextView mTextView;
+        }
+    }
+}
